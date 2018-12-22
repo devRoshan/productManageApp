b689dc16e9cef72ba38695ee3448d0dd82b003a2
Roshan <rcroshan63@gmail.com>
2018-12-22 20:38:02 +0545

    dissmissed product and restructed the code

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -5,50 +5,61 @@ import './product_edit.dart';
 class ProductListPage extends StatelessWidget {
   final List<Map<String, dynamic>> products;
   final Function updateProduct;
+  final Function deleteProduct;
 
-  ProductListPage(this.products, this.updateProduct);
+  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) {
-                  return ProductEditPage(
-                    product: products[index],
-                    updateProduct: updateProduct, 
-                    productIndex: index
-                  );
-                }
-              )
-            );
+        return Dismissible(
+          key: Key(products[index]['title']),
+          background: Container(
+            color: Colors.red,
+          ),
+          onDismissed: (DismissDirection direction) {
+            if (direction == DismissDirection.endToStart) {
+              this.debugDescribeChildren();
+            } else if (direction == DismissDirection.startToEnd) {
+              print('Start to end');
+            } else {
+              print('other swiping direction');
+            }
           },
-          child: ListTile(
-            leading: Image.asset(products[index]['image']),
-            title: Text(products[index]['title']),
-            trailing: IconButton(
-              icon: Icon(Icons.edit),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return ProductEditPage(
-                        product: products[index],
-                        updateProduct: updateProduct, 
-                        productIndex: index
-                      );
-                    }
-                  )
-                );
-              },
-            ),
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(backgroundImage: AssetImage(products[index]['image'])),
+                title: Text(products[index]['title']),
+                subtitle: Text('\$${products[index]['price']}'),
+                trailing: _buildEditButton(context, index)
+              ),
+              Divider()  
+            ]
           )
         );
       },
       itemCount: products.length,
+    );
+  }
+
+  Widget _buildEditButton(BuildContext context, index) {
+    return IconButton(
+      icon: Icon(Icons.edit),
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (BuildContext context) {
+              return ProductEditPage(
+                product: products[index],
+                updateProduct: updateProduct, 
+                productIndex: index
+              );
+            }
+          )
+        );
+      },
     );
   }
 }
